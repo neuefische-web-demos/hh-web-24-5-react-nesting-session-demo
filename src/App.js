8df9349599139f40d7ca05e54a086dc986002590
Jessica Lörs <jessica.loers@gmail.com>
2024-06-24 10:39:59 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -6,18 +6,38 @@ export default function App() {
     <main>
       <h1>Animal Shelter</h1>
       <p>Please adopt our lovely pets!</p>
-      <Animal emoji="🐕" name="Lucky" />
-      <Animal emoji="🐈" name="Mittens" />
-      <Animal emoji="🐢" name="Archibald" />
-      <Animal emoji="🐣" name="Chick Norris" />
+      <Animal emoji="🐳" name="klaus">
+        <ul>
+          <li>cute</li>
+          <li>smart</li>
+          <li>nice</li>
+        </ul>
+      </Animal>
+      <Animal emoji="🐕" name="Lucky">
+        <p>Cute dog</p>
+      </Animal>
+      <Animal emoji="🐈" name="Mittens">
+        <p>Super cute cat</p>
+      </Animal>
+      <Animal emoji="🐣" name="Chick Norris">
+        <ul>
+          <li>cute</li>
+          <li>smart</li>
+          <li>nice</li>
+        </ul>
+      </Animal>
     </main>
   );
 }
-
-function Animal({ emoji, name }) {
+// <> ... </> Fragements - <React.Fragments> ...  </React.Fragments>
+function Animal({ emoji, name, children }) {
+  console.log('children', children);
   return (
-    <h2>
-      {emoji} {name}
-    </h2>
+    <>
+      <h2>
+        {emoji} {name}
+      </h2>
+      {children}
+    </>
   );
 }
